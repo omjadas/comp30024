949a007bcd39740a4e75d5868e1f87e08a39ad78
omjadas <omja.das@gmail.com>
2018-03-23 15:20:12 +1100

    implemented count_moves method in Game

--- ass1.py
+++ ass1.py
@@ -4,8 +4,8 @@ class Game:
 
     def __init__(self):
         self.read_board()
-        self.white = Player(self.layout, "O")
-        self.black = Player(self.layout, "@")
+        self.white_player = Player(self.layout, "O")
+        self.black_player = Player(self.layout, "@")
 
     def read_board(self):
         """Read the board layout from input"""
@@ -18,7 +18,45 @@ class Game:
         pass
 
     def count_moves(self):
-        pass
+        white_moves = 0
+        black_moves = 0
+        for i in self.white_player.pieces:
+            if i[0] > 0 and self.layout[i[0] - 1][i[1]] == "-":
+                white_moves += 1
+            if i[1] > 0 and self.layout[i[0]][i[1] - 1] == "-":
+                white_moves += 1
+            if i[0] < 7 and self.layout[i[0] + 1][i[1]] == "-":
+                white_moves += 1
+            if i[1] < 7 and self.layout[i[0]][i[1] + 1] == "-":
+                white_moves += 1
+            if i[0] > 1 and self.layout[i[0] - 1][i[1]] in ["O","@"] and self.layout[i[0] - 2][i[1]] == "-":
+                white_moves += 1
+            if i[1] > 1 and self.layout[i[0]][i[1] - 1] in ["O","@"] and self.layout[i[0]][i[1] - 2] == "-":
+                white_moves += 1
+            if i[0] < 6 and self.layout[i[0] + 1][i[1]] in ["O","@"] and self.layout[i[0] + 2][i[1]] == "-":
+                white_moves += 1
+            if i[1] < 6 and self.layout[i[0]][i[1] + 1] in ["O","@"] and self.layout[i[0]][i[1] + 2] == "-":
+                white_moves += 1
+        for i in self.black_player.pieces:
+            if i[0] > 0 and self.layout[i[0] - 1][i[1]] == "-":
+                black_moves += 1
+            if i[1] > 0 and self.layout[i[0]][i[1] - 1] == "-":
+                black_moves += 1
+            if i[0] < 7 and self.layout[i[0] + 1][i[1]] == "-":
+                black_moves += 1
+            if i[1] < 7 and self.layout[i[0]][i[1] + 1] == "-":
+                black_moves += 1
+            if i[0] > 1 and self.layout[i[0] - 1][i[1]] in ["O","@"] and self.layout[i[0] - 2][i[1]] == "-":
+                black_moves += 1
+            if i[1] > 1 and self.layout[i[0]][i[1] - 1] in ["O","@"] and self.layout[i[0]][i[1] - 2] == "-":
+                black_moves += 1
+            if i[0] < 6 and self.layout[i[0] + 1][i[1]] in ["O","@"] and self.layout[i[0] + 2][i[1]] == "-":
+                black_moves += 1
+            if i[1] < 6 and self.layout[i[0]][i[1] + 1] in ["O","@"] and self.layout[i[0]][i[1] + 2] == "-":
+                black_moves += 1
+        print(white_moves)
+        print(black_moves)
+        return None
 
     def massacre(self):
         pass
@@ -42,8 +80,8 @@ class Player:
     def __init__(self, layout, player):
         self.pieces = []
         for i in range(len(layout)):
-            for j in range(len(i)):
-                if j == player:
+            for j in range(len(layout[i])):
+                if layout[i][j] == player:
                     self.pieces.append([i, j])
 
 
@@ -58,7 +96,6 @@ class Node:
 
 
 game = Game()
-game.read_board()
 if (input() == "Moves"):
     game.count_moves()
 else:
