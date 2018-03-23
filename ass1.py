9bc08272f948cde2b20de1b55059d800e41c0eaa
omjadas <omja.das@gmail.com>
2018-03-23 14:09:35 +1100

    added Game and Node class and some function declarations

--- ass1.py
+++ ass1.py
@@ -1,4 +1,4 @@
-class Board:
+class Game:
     """
     """
     layout = []
@@ -7,10 +7,33 @@ class Board:
         pass
 
     def read_board(self):
-        """Read the board layout from input"""
-        for _ in range(8):
-            self.layout.append(list(input()))
-        return None
+    """Read the board layout from input"""
+    for _ in range(8):
+        self.layout.append(list(input()))
+    return None
+
+    def make_move(self):
+        pass
+    
+    def count_moves():
+        pass
+
+    def massacre(self):
+        pass
+
+    def moves(self):
+        pass
+
+
+
+class Board:
+    """
+    """
+    layout = []
+
+    def __init__(self, layout):
+        self.layout = layout
+        return none
 
     def make_move(self):
         pass
@@ -19,6 +42,20 @@ class Board:
 class Player:
     """
     """
+    pieces = []
 
     def __init__(self):
         pass
+
+
+class Node:
+    """
+    """
+
+
+game = new Game()
+game.read_board()
+if (input() == "Moves"):
+    game.moves()
+else:
+    game.massacre()
